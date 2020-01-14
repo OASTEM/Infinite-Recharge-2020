f99ae835f0edd39dc7ed1b7077020eed79b6eb12
Justin <flyingjay16@gmail.com>
2020-01-13 16:03:32 -0800

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public static final int driveFrontLeftID = 0;
+    public static final int driveFrontRightID = 1;
+    public static final int driveBackLeftID = 2;
+    public static final int driveBackRightID = 3;
+
+    public static final int dPosFactor = 42;
+
+    public static final double dMinOutput = 1.0;
+    public static final double dMaxOutput = -1.0;
+
+    public static final int dPos_Slot = 0;
+    public static final double dPos_kP = 1.0;
+    public static final double dPos_kI = 0.0;
+    public static final double dPos_kD = 0.0;
+    public static final double dPos_kF = 0.0;
+
+    public static final double dClosedLoop_Ramp = 1.0;
+    public static final double dOpenLoop_Ramp = 1.0;
+
+    public static final int dSmart_Motion_Slot = 1;
+    public static final double dSmart_Motion_kP = 1.0;
+    public static final double dSmart_Motion_kI = 0.0;
+    public static final double dSmart_Motion_kD = 0.0;
+    public static final double dSmart_Motion_KF = 0.0;
+
+    public static final int dSmart_Motion_Min_Accel = 100;
+    public static final int dSmart_Motion_Min_Velocity = 100;
+    public static final int dSmart_motion_Max_Velocity = 200; 
+
+    public static final int dSmart_Motion_Allowed_Error = 100;    
+}
+
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,122 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.EncoderType;
+import com.revrobotics.CANPIDController.AccelStrategy;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants;
+
+public class DriveTrain extends SubsystemBase {
+  /**
+   * Creates a new DriveTrain.
+   */
+  CANSparkMax frontLeft; 
+  CANSparkMax frontRight;
+  CANSparkMax backLeft;
+  CANSparkMax backRight;
+
+  CANPIDController leftController;
+  CANPIDController rightController;
+
+  CANEncoder leftEncoder;
+  CANEncoder rightEncoder;
+
+  public DriveTrain() {
+    frontLeft = new CANSparkMax(1, MotorType.kBrushless);
+    frontRight = new CANSparkMax(2, MotorType.kBrushless);
+    backLeft = new CANSparkMax(3, MotorType.kBrushless);
+    backRight = new CANSparkMax(4, MotorType.kBrushless);
+
+    leftEncoder = frontLeft.getEncoder();
+    rightEncoder = frontRight.getEncoder();
+
+    //leftEncoder = frontLeft.getEncoder(EncoderType.kHallSensor, 42);
+    //rightEncoder = frontRight.getEncoder(EncoderType.kHallSensor, 42);
+
+    backLeft.follow(frontLeft);
+    backRight.follow(frontRight);
+    
+    frontLeft.setInverted(false);
+    frontRight.setInverted(false);
+
+    //sets PID gains for position control for the left pid controller
+    leftController.setP(Constants.dPos_kP, Constants.dPos_Slot);
+    leftController.setI(Constants.dPos_kI, Constants.dPos_Slot);
+    leftController.setD(Constants.dPos_kD, Constants.dPos_Slot);
+    leftController.setFF(Constants.dPos_kF, Constants.dPos_Slot);
+ 
+    //sets PID gains for position control for the right pid controller
+    rightController.setP(Constants.dPos_kP, Constants.dPos_Slot);
+    rightController.setI(Constants.dPos_kI, Constants.dPos_Slot);
+    rightController.setD(Constants.dPos_kD, Constants.dPos_Slot);
+    rightController.setFF(Constants.dPos_kF, Constants.dPos_Slot);
+
+    frontLeft.setOpenLoopRampRate(Constants.dOpenLoop_Ramp);
+    frontRight.setOpenLoopRampRate(Constants.dOpenLoop_Ramp);
+
+    frontLeft.setClosedLoopRampRate(Constants.dClosedLoop_Ramp);
+    frontRight.setClosedLoopRampRate(Constants.dClosedLoop_Ramp);
+
+     //configure necessary smart motion parameters for the left PID controllers
+     leftController.setSmartMotionAccelStrategy(AccelStrategy.kTrapezoidal, Constants.dSmart_Motion_Slot);
+     leftController.setSmartMotionMaxAccel(Constants.dSmart_Motion_Min_Accel, Constants.dSmart_Motion_Slot);
+     leftController.setSmartMotionMinOutputVelocity(Constants.dSmart_Motion_Min_Velocity, Constants.dSmart_Motion_Slot);
+     leftController.setSmartMotionMaxVelocity(Constants.dSmart_motion_Max_Velocity, Constants.dSmart_motion_Max_Velocity);
+ 
+     //configure necessary smart motion parameters for the right PID controllers
+     rightController.setSmartMotionAccelStrategy(AccelStrategy.kTrapezoidal, Constants.dSmart_Motion_Slot);
+     rightController.setSmartMotionMaxAccel(Constants.dSmart_Motion_Min_Accel, Constants.dSmart_Motion_Slot);
+     rightController.setSmartMotionMinOutputVelocity(Constants.dSmart_Motion_Min_Velocity, Constants.dSmart_Motion_Slot);
+     rightController.setSmartMotionMaxVelocity(Constants.dSmart_motion_Max_Velocity, Constants.dSmart_motion_Max_Velocity);
+ 
+     //set PID gains for smart motion
+     leftController.setP(Constants.dSmart_Motion_kP, Constants.dSmart_Motion_Slot);
+     leftController.setI(Constants.dSmart_Motion_kI, Constants.dSmart_Motion_Slot);
+     leftController.setD(Constants.dSmart_Motion_kD, Constants.dSmart_Motion_Slot);
+     leftController.setFF(Constants.dSmart_Motion_KF, Constants.dSmart_Motion_Slot);
+ 
+     rightController.setP(Constants.dSmart_Motion_kP, Constants.dSmart_Motion_Slot);
+     rightController.setI(Constants.dSmart_Motion_kI, Constants.dSmart_Motion_Slot);
+     rightController.setD(Constants.dSmart_Motion_kD, Constants.dSmart_Motion_Slot);
+     rightController.setFF(Constants.dSmart_Motion_KF, Constants.dSmart_Motion_Slot);
+ 
+     leftController.setSmartMotionAllowedClosedLoopError(Constants.dSmart_Motion_Allowed_Error, Constants.dSmart_Motion_Slot);
+     rightController.setSmartMotionAllowedClosedLoopError(Constants.dSmart_Motion_Allowed_Error, Constants.dSmart_Motion_Slot);
+ 
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void drivePercentOutput(double left, double right) {
+    frontLeft.set(-left); //since the motors face opposite directions
+    frontRight.set(right);
+  }
+
+  public void drivePosition(double goal) {
+    leftController.setReference(goal, ControlType.kPosition, Constants.dPos_Slot);
+    rightController.setReference(goal, ControlType.kPosition, Constants.dPos_Slot);
+  }
+
+  public void driveSmartMotion(double goal) {
+    leftController.setReference(goal, ControlType.kSmartMotion, Constants.dSmart_Motion_Slot);
+    rightController.setReference(goal, ControlType.kSmartMotion, Constants.dSmart_Motion_Slot);
+  }
+  
+}
